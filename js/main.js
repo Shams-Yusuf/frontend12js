aac0e0b922222ccd86ab83b8c27f1244eb0f66a3
shams yusuf <shamsudinvu@gmail.com>
2023-07-24 10:44:03 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//написать два переменных с правильным именем
+let navBar = "menu";
+let test22 = 21;
+// написать два переменных с неправильным именем
+//let Nav bar = "menu";
+//let 22test = 21;
+
+console.log(navBar, test22);
